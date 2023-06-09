a17144cf4a80662ee485c5aaef0378d30e37de77
Demanuel001 <daniloemanuel360@gmail.com>
2023-06-08 23:14:11 -0300

    inicia o projeto

---
+++ src/config/colors.js
@@ -0,0 +1,11 @@
+export default {
+    background: '#121214',
+    text: '#C5C4D0',
+    placeholder: '#7C7C8A',
+    bntLogin: '#2B3891',
+    bgFundoMesas: '#202024',
+    bgMesa: '#4F4F4F',
+    bgInput: '#202024',
+    // ... outras cores
+  };
+  
